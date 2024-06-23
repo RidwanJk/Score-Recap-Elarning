82c04db56533d5bea67dee9c4905086254fe5be8
Ridwan Jauhar Kafabihi <145266609+RidwanJk@users.noreply.github.com>
2024-06-24 06:10:38 +0700

    Changes to be committed: 	deleted:    data.xlsx 	modified:   main.py 	new file:   readme.md

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import pandas as pd
+import argparse
 from password import username, password
 
 def login(session, login_url):    
@@ -43,15 +44,20 @@ def get_data(html):
     
     return headers, data
 
-def save_to_excel(headers, data, filename):
+def save_to_excel(headers, data, filename, directory='Data'):
     df = pd.DataFrame(data, columns=headers)
-    df.to_excel(filename, index=False)
+    df.to_excel(f'{directory}/{filename}.xlsx', index=False)
+    print(f"Data saved to {directory}/{filename}.xlsx")
 
 def main():
+    parser = argparse.ArgumentParser(description='Scrape and save data from elearning site.')
+    parser.add_argument('filename', type=str, help='The output filename for the Excel file.')
+    parser.add_argument('url', type=str, help='The url for the score.')
+    args = parser.parse_args()
+
     login_url = 'https://elearning.uin-malang.ac.id/login/index.php'  
-    url = 'https://elearning.uin-malang.ac.id/mod/assign/view.php?id=92790&action=grading'
-    page2= url + "&page=1"
-    
+    url = args.url 
+    page2 = url + "&page=1"
     
     with requests.Session() as session:        
         login_response = login(session, login_url)
@@ -59,10 +65,10 @@ def main():
             html = get_html(session, url)
             html2 = get_html(session, page2)        
             headers, data = get_data(html)
-            headers, data2 = get_data(html2)
+            headers2, data2 = get_data(html2)
             data.extend(data2)
             if headers and data:
-                save_to_excel(headers, data)
+                save_to_excel(headers, data, args.filename)
             else:
                 print("No data to save.")
         else:
---
+++ readme.md
@@ -0,0 +1,32 @@
+# App Name
+
+## Description
+
+This app is designed to provide a score recap for e-learning activities. It allows users to track their progress and view a summary of their scores.
+
+## Features
+
+- Score tracking: Keep track of scores for different e-learning activities.
+- Summary view: View a recap of scores for each activity.
+- User-friendly interface: The app provides an intuitive and easy-to-use interface.
+
+## Installation
+
+1. Clone the repository.
+2. Open the app in your preferred code editor.
+
+
+## Usage
+
+Run the code using script 
+
+python main.py output_filename.xlsx
+
+
+## Contributing
+
+Contributions are welcome! If you have any suggestions or improvements, please submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
